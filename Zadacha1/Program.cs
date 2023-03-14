ec287df711c7fd8a563b1514bbd02ba5a6283d7e
Daniil <puzikovdaniil1@outlook.com>
2023-03-14 19:36:08 +0300

    Первый коммит

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a);
+int max = a;
+int min = b;
+if (b>max) 
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine("Максимальное число равно " + max);
+Console.WriteLine("Минимальное число равно " + min);
+
---
+++ Zadacha5/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 7 HARD по желанию - идет за 2 необязательных Напишите программу, которая принимает на вход целое число любой разрядности и на выходе показывает вторую цифру слева этого числа или говорит, что такой цифры нет. Через строку решать нельзя.
+//456111 -> 5
+//78 -> 8
+//9146548 -> 1
+//3 -> нет
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a;
+int size = 0;
+while (b>0)
+{
+    b = b/10;
+    size++;
+}
+b = a;
+int[] array = new int[size];
+int index = 0;
+while (index<size)
+{
+    array[index] = b%10;
+    b = b/10;
+    index++;
+}
+if (size-2<0)
+{
+    Console.WriteLine("Второй цифры нет");
+}
+else
+Console.WriteLine("Вторая цифра в числе - это " + array[size-2]);
+
